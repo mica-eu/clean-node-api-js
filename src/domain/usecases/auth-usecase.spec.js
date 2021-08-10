61736887ae5d3b6e0e51ff7bd3b11cdd49f5ec38
Micael Souza <micael.souza@outlook.com>
2021-08-10 10:54:07 -0300

    ensure AuthUseCase call LoadUserByEmailRepository with correct email

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,6 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable max-classes-per-file */
 const { MissingParamError } = require('../../utils/errors');
 
 class AuthUseCase {
+  constructor(loadUserByEmailRepository) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository;
+  }
+
   // eslint-disable-next-line class-methods-use-this
   async auth(email, password) {
     if (!email) {
@@ -9,19 +16,38 @@ class AuthUseCase {
     if (!password) {
       throw new MissingParamError('password');
     }
+    await this.loadUserByEmailRepository.load(email);
   }
 }
 
+const makeSut = () => {
+  class LoadUserByEmailRepositorySpy {
+    async load(email) {
+      return null;
+    }
+  }
+  const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy();
+  const sut = new AuthUseCase(loadUserByEmailRepositorySpy);
+  return { sut, loadUserByEmailRepositorySpy };
+};
+
 describe('AuthUseCase', () => {
   test('Should throw if no email is provided', async () => {
-    const sut = new AuthUseCase();
+    const { sut } = makeSut();
     const promise = sut.auth();
     expect(promise).rejects.toThrow(new MissingParamError('email'));
   });
 
   test('Should throw if no password is provided', async () => {
-    const sut = new AuthUseCase();
+    const { sut } = makeSut();
     const promise = sut.auth('valid_email@email.com');
     expect(promise).rejects.toThrow(new MissingParamError('password'));
+  });
+
+  test('Should call LoadUserByEmailRepository with correct', async () => {
+    const { sut, loadUserByEmailRepositorySpy } = makeSut();
+    const loadSpy = jest.spyOn(loadUserByEmailRepositorySpy, 'load');
+    await sut.auth('valid_email@email.com', 'any_password');
+    expect(loadSpy).toBeCalledWith('valid_email@email.com');
   });
 });
