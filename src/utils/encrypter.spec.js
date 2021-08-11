8ce92ac4ce839739b629ad22f5bca5eddbfdd3a7
Micael Souza <micael.souza@outlook.com>
2021-08-10 21:58:26 -0300

    ensure Encrypter calls bcrypt with correct values

--- src/utils/encrypter.spec.js
+++ src/utils/encrypter.spec.js
@@ -21,4 +21,11 @@ describe('Encrypter', () => {
     const isValid = await sut.compare('any_value', 'hash');
     expect(isValid).toBe(false);
   });
+
+  test('Shuld call bcrypt with correct values', async () => {
+    const sut = new Encrypter();
+    const compareSpy = jest.spyOn(bcrypt, 'compare');
+    await sut.compare('any_value', 'hash_value');
+    expect(compareSpy).toBeCalledWith('any_value', 'hash_value');
+  });
 });
