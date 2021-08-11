7e9a7565e57873c6055b4bf54b18c9a94c82dd08
Micael Souza <micael.souza@outlook.com>
2021-08-11 12:00:47 -0300

    ensure TokenGenerator returns a token if jwt returns a token

---
+++ __mocks__/jsonwebtoken.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+module.exports = {
+  sign(id, secret) {
+    return 'valid_jsonwebtoken';
+  },
+};
--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,9 +1,9 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable class-methods-use-this */
+const jwt = require('jsonwebtoken');
 
 class TokenGenerator {
-  async generate(_id) {
-    return null;
+  async generate(id) {
+    return jwt.sign(id, 'secret');
   }
 }
 
@@ -15,7 +15,15 @@ const makeSut = () => {
 describe('TokenGenerator', () => {
   test('Should return null if jwt return null', async () => {
     const { sut } = makeSut();
+    jest.spyOn(jwt, 'sign').mockReturnValueOnce(null);
     const token = await sut.generate('any_id');
     expect(token).toBeNull();
+  });
+
+  test('Should return a token if jwt returns jwt', async () => {
+    const { sut } = makeSut();
+    jest.spyOn(jwt, 'sign').mockReturnValueOnce('valid_jsonwebtoken');
+    const token = await sut.generate('any_id');
+    expect(token).toBe('valid_jsonwebtoken');
   });
 });
