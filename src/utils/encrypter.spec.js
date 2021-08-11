125e96b65be9ce6f8234a001c1b3ee70b4ddd69b
Micael Souza <micael.souza@outlook.com>
2021-08-10 22:00:11 -0300

    move sut creation to a factory helper method

--- src/utils/encrypter.spec.js
+++ src/utils/encrypter.spec.js
@@ -7,23 +7,28 @@ class Encrypter {
   }
 }
 
+const makeSut = () => {
+  const sut = new Encrypter();
+  return { sut };
+};
+
 describe('Encrypter', () => {
   test('Shuld return true if bcrypt return true', async () => {
-    const sut = new Encrypter();
+    const { sut } = makeSut();
     jest.spyOn(bcrypt, 'compare').mockResolvedValueOnce(true);
     const isValid = await sut.compare('any_value', 'hashed_value');
     expect(isValid).toBe(true);
   });
 
   test('Shuld return false if bcrypt return false', async () => {
-    const sut = new Encrypter();
+    const { sut } = makeSut();
     jest.spyOn(bcrypt, 'compare').mockResolvedValueOnce(false);
     const isValid = await sut.compare('any_value', 'hash');
     expect(isValid).toBe(false);
   });
 
   test('Shuld call bcrypt with correct values', async () => {
-    const sut = new Encrypter();
+    const { sut } = makeSut();
     const compareSpy = jest.spyOn(bcrypt, 'compare');
     await sut.compare('any_value', 'hash_value');
     expect(compareSpy).toBeCalledWith('any_value', 'hash_value');
