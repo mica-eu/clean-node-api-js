81588358c27b44a1fe427c45533a17295952d358
Micael Souza <micael.souza@outlook.com>
2021-08-25 17:24:04 -0300

    enable CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -4,7 +4,15 @@ const app = require('./app');
 describe('App Setup', () => {
   test('Should disable x-powered-by header', async () => {
     app.get('/test-x-powered-by', (req, res) => res.send(''));
-    const res = await request(app).get('/');
+    const res = await request(app).get('/test-x-powered-by');
     expect(res.header['x-powered-by']).toBeUndefined();
+  });
+
+  test('Should enable CORS', async () => {
+    app.get('/test-cors', (req, res) => res.send(''));
+    const res = await request(app).get('/test-cors');
+    expect(res.header['access-control-allow-origin']).toBe('*');
+    expect(res.header['access-control-allow-methods']).toBe('*');
+    expect(res.header['access-control-allow-headers']).toBe('*');
   });
 });
