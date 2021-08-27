4820ecd9a76c1e3fc58ab6bb416545286bc59c1a
Micael Souza <micael.souza@outlook.com>
2021-08-26 22:34:44 -0300

    connect mongodb before listen for app

--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,15 @@
+const console = require('console');
+const MongoHelper = require('../infra/helpers/mongo-helper');
 const app = require('./config/app');
+const { mongoUrl } = require('./config/env');
 
-app.listen(5858, () => console.log('[server] running'));
+(async () => {
+  try {
+    const db = new MongoHelper(mongoUrl);
+    await db.connect();
+    app.listen(5858, () => console.log('[server] running'));
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})();
